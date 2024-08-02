4692e55e605a6f1937ca93af7941cf622b890f64
znorgaard <zach@fulcrumgenomics.com>
2024-08-02 08:14:18 -0700

    chore: remove unused function

--- longplexpy/lima/__init__.py
+++ longplexpy/lima/__init__.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Iterable
 
 from fgpyo.util.metric import Metric
 
@@ -34,18 +33,3 @@ class LimaReportMetric(Metric["LimaReportMetric"]):
             return HYBRID_STATUS
         else:
             return PASS_STATUS
-
-
-def validate_header(input_fieldnames: Iterable[str], required_fields: Iterable[str]) -> None:
-    """Validates that a header has the required fields
-
-    Args:
-        input_fieldnames: list of fieldnames in input header
-        required_fields: list of required fields.
-            Default = REPORT_FIELDS
-    Raises:
-        ValueError if any required fields are not fond in the input fieldnames
-    """
-    missing_fields = [field for field in required_fields if field not in input_fieldnames]
-    if len(missing_fields) != 0:
-        raise ValueError(f"Input is missing required fields: {', '.join(missing_fields)}")
